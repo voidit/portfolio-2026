64f47efaf504e5ea63bca4c8bc8cf9e66f870ddd
Irena Romendik <kidnemor@gmail.com>
2025-09-27 10:55:45 +0530

    fixes for build errors

---
+++ docs/_app/immutable/nodes/9.14d22b03.js
@@ -0,0 +1,1 @@
+import{P as m}from"../chunks/9.e120c14b.js";export{m as component};
--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 		adapter: adapter({
 			pages: 'docs',
 			assets: 'docs',
-			fallback: null,
+			fallback: 'index.html', // Add this line
 			precompress: false,
 			strict: true
 		}),
